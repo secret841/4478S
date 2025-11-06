0b0a1652da98d8c899a7d8e6a946acc98c3d42d9
Secret84 <lamp72020@gmail.com>
2025-11-05 21:27:13 -0500

    Daniel Hand Bot

--- log_drive/include/motors.h
+++ log_drive/include/motors.h
@@ -4,8 +4,8 @@
 //Defining motors and controller
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 
-pros::MotorGroup left_motor ({-1, -2, -3}, pros::MotorGearset::blue);
-pros::MotorGroup right_motor({4, 12, 13}, pros::MotorGearset::blue); 
+pros::MotorGroup left_motor ({-10, -19, -20}, pros::MotorGearset::blue);
+pros::MotorGroup right_motor({1, 13, 12}, pros::MotorGearset::blue); 
 
 pros::Imu Inertial(5);
 
